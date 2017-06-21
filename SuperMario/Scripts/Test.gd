af229c21ade718696636c798502bc42de7e3dad6
matthiascy <matthias_cy@outlook.com>
2017-06-21 13:38:48 +0200

    tmp commit

---
+++ SuperMario/Scripts/Test.gd
@@ -0,0 +1,14 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process(true)
+	
+func _process(delta):
+	VisualServer.set_default_clear_color(Color(0.0, 0.0, 0.0, 1.0))
+	
